1eda45fa3d67de6e09f292ebb79b67e66847b7f7
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-21 06:20:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,4 +40,10 @@ class InteractiveRecord
     end
     values.join(", ")
   end
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0
+  end
 end
